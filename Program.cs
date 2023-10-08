f039be9aef2dd2da85226ec6f5368c496d0a78b0
TheDolphin <dolphinboy12213@gmail.com>
2023-10-08 21:08:03 +0000

    wee

--- Program.cs
+++ Program.cs
@@ -8,15 +8,20 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {   
+            Random rnd = new Random();
             Console.Title = "YOUR MOTHER";
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("It seems to be a dull day today. You have two options.");
-            Console.WriteLine("\n  1. Search your room.    2. Wither away.   ");
+            
+            Console.WriteLine("It seems to be a dull morning today. Something nags you in your head, but you push off your worries to the side. \n ");
+            
+            Console.WriteLine("\n  1. Get out of bed.    2. Wither away.   ");
+            
             int decision1 = Convert.ToInt32(Console.ReadLine());
             
             if (decision1 == 1)
             {
-                Console.WriteLine("\nYou get out of your bed and find\n");
+                Console.WriteLine("\nYou get out of your bed and find that your room is way colder than it should be. Strange.\n");
+                Console.ReadKey();
             }
             if (decision1 == 2)
             {
@@ -24,6 +29,19 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 Console.ReadKey();
                 return;
             }
+
+            Console.WriteLine(" 1. Open your closet.  2. Investigate Your room.(D20) \n");
+            int decision2 = Convert.ToInt32(Console.ReadKey());
+            if (decision2 == 1)
+            {
+
+            }
+            if (decision2 == 2)
+            {
+                Console.WriteLine("wee");
+            }
+
+            
         }
     }
 }
