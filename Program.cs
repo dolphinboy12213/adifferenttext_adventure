d54ace1f5544908a79df096dd1e68f8df5aeccdc
TheDolphin <dolphinboy12213@gmail.com>
2023-10-09 00:56:16 +0000

    wee

--- Program.cs
+++ Program.cs
@@ -11,10 +11,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
             Random rnd = new Random();
             Console.Title = "YOUR MOTHER";
             Console.ForegroundColor = ConsoleColor.Green;
-            
+            Console.WriteLine("\nYou wake up.\n");
             Console.WriteLine("It seems to be a dull morning today. Something nags you in your head, but you push off your worries to the side. \n ");
             
-            Console.WriteLine("\n  1. Get out of bed.    2. Wither away.   \n");
+            Console.WriteLine("\n  1. Get out of bed.    2. Go back to sleep.   \n");
             
             int decision1 = Convert.ToInt32(Console.ReadLine());
             
@@ -25,7 +25,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
             }
             if (decision1 == 2)
             {
-                Console.WriteLine("\nAs you take your last breaths, you start to realize what a stupid decision you've made.\n\n Press any key to continue. \n");
+                Console.WriteLine("\nYou pass out and suffocate on your pillow.\n\n Press any key to continue. \n");
                 Console.ReadKey();
                 return;
             }
@@ -34,16 +34,35 @@ namespace MyApp // Note: actual namespace depends on the project name.
             int decision2 = Convert.ToInt32(Console.ReadLine());
             if (decision2 == 1)
             {
-
+                Console.WriteLine("\nYou open your closet and get dressed for the day ahead.");
             }
             if (decision2 == 2)
             {
-                int intelligente = rnd.Next(1 , 20);
-                Console.WriteLine("You rolled a: " + intelligente);
+                Console.WriteLine("\n Press any key to roll...");
+                Console.ReadKey();
+                Console.WriteLine("\n Rolling craft (d20)...");
+                int craft = rnd.Next(1 , 20);
+                Console.WriteLine("\n You rolled a: " + craft);
+                Console.ReadKey();
 
-                if (intelligente == 1)
+                if (craft == 1)
                 {
-                    
+                    Console.WriteLine("\nAs you search the room, You accidentally slip on a perfectly placed banana peel.");
+                    Console.WriteLine("\nTake two damage");
+                }
+                if (craft == 20)
+                {
+                    Console.WriteLine("\nAs you search the room, you find a really rusty crowbar under your bed and a very suspicisous banana peel.");
+                    // add banana peel and crowbar to inv
+                }
+                if (craft >= 2 && craft <= 10)
+                {
+                    Console.WriteLine("\nYou find nothing of real interest.");
+                }
+                if (craft >= 10 && craft <= 19)
+                {
+                    Console.WriteLine("\nYou find a really rusty crowbar under your bed.");
+                    //add crowbar to inv
                 }
             }
 
