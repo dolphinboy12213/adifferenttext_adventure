1a8ff815a69a12cb9c622f8662958ced93d9a777
TheDolphin <dolphinboy12213@gmail.com>
2023-10-11 22:43:37 +0000

    add space (organize)

--- Program.cs
+++ Program.cs
@@ -10,16 +10,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
     internal class Program
     {
 
-
-
-
-
-
-        
-        
-         
-
-
+    
         static void Main(string[] args)
         {
             string[] inventory = new string[4];
